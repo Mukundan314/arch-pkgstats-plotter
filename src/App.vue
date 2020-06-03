ed51cd6207da634e1dab8467413761b33b5ae066
Mukundan Senthil <mukundan314@gmail.com>
2020-06-03 15:01:38 +0530

    Create PackageList component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+    <package-list v-model="packageNames" />
   </div>
 </template>
 
 <script>
+import PackageList from './components/PackageList.vue';
+
 export default {
   name: 'App',
+  data () {
+    return {
+      packageNames: [],
+    }
+  },
   components: {
+    PackageList,
   },
 };
 </script>
@@ -19,5 +28,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 10%;
+  margin-right: 10%;
 }
 </style>
---
+++ src/components/PackageList.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="package-list">
+    <div class="heading">Packages</div>
+
+    <div v-for="(packageName, idx) in packageNames" :key="packageName" class="package">
+      <span class="package-name">{{ packageName }}</span>
+      <button v-on:click="removePackage(idx)">remove</button>
+    </div>
+
+    <form class="package" v-on:submit.prevent="addPackage">
+      <input v-model="addPackageName" class="package-name" placeholder="Package" />
+      <button>add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'packageNames',
+  },
+  props: {
+    packageNames: {
+      type: Array,
+      default: () => [],
+      validator: (value) => (new Set(value)).size === value.length,
+    },
+  },
+  data () {
+    return { addPackageName: "" };
+  },
+  methods: {
+    addPackage() {
+      if (!this.packageNames.includes(this.addPackageName)) {
+        this.$emit('input', [...this.packageNames, this.addPackageName]);
+        this.addPackageName = '';
+      }
+    },
+    removePackage(idx) {
+      this.$emit('input', this.packageNames.filter((_, i) => i != idx));
+    },
+  }
+};
+</script>
+
+<style scoped>
+.package-list {
+  text-align: left;
+
+  width: 25%;
+  min-width: 15rem;
+
+  border: solid 1px #dbdbdb;
+  border-radius: .5rem;
+
+  overflow: hidden;
+}
+
+.heading, .package {
+  padding: .7rem 1rem;
+}
+
+.heading {
+  font-size: 1.1rem;
+  background: #f5f5f5;
+}
+
+.package {
+  display: flex;
+  border-top: solid 1px #dbdbdb;
+  font-size: 0.9rem;
+}
+
+.package-name {
+  overflow: auto;
+}
+
+input {
+  width: 100%;
+  border: 0;
+}
+
+button {
+  background: none;
+
+  margin-left: auto;
+  font-size: .75rem;
+
+  box-shadow: none;
+
+  border: solid 1px #dbdbdb;
+  border-radius: .2rem;
+}
+
+button:hover {
+  border: solid 1px #b5b5b5;
+}
+</style>
